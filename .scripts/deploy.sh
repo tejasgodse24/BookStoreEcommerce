eb3a18d1087ee656db69375be16605f736867cb0
tejas24 <tejasgodse19@gmail.com>
2024-09-28 13:10:06 +0530

    autodeploy files added

---
+++ .scripts/deploy.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set -e
+
+echo "Deployment started ..."
+
+# Pull the latest version of the app
+echo "Copying New changes...."
+git pull origin main
+echo "New changes copied to server !"
+
+
+# copying  media files to media folder
+echo "Copying css files...."
+cp -r /home/ubuntu/BookStoreEcommerce/public/media/* /var/www/bookstore/media
+
+
+# Activate Virtual Env
+#Syntax:- source virtual_env_name/bin/activate
+source venv/bin/activate
+echo "Virtual env 'venv' Activated !"
+
+echo "Installing Dependencies..."
+pip install -r requirements.txt --no-input
+
+echo "Clearing Cache..."
+python manage.py clean_pyc
+python manage.py clear_cache
+
+echo "Serving Static Files..."
+python manage.py collectstatic --noinput
+
+echo "Running Database migration..."
+# python manage.py makemigrations
+python manage.py migrate
+
+# Deactivate Virtual Env
+deactivate
+echo "Virtual env 'venv' Deactivated !"
+
+
+
+# echo "reloading celery and celery beat"
+# sudo supervisorctl restart all
+
+# echo "reloading redis server"
+# sudo systemctl restart redis-server
+
+echo "Reloading App..."
+#kill -HUP `ps -C gunicorn fch -o pid | head -n 1`
+ps aux |grep gunicorn |grep bookShop | awk '{ print $2 }' |xargs kill -HUP
+
+echo "Deployment Finished !"
