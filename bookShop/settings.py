5f61177c0b0fcbf5e3028a5d59aa411219539b8d
tejas24 <tejasgodse19@gmail.com>
2024-11-10 00:10:06 +0530

    2nd commit for celery (celery_boker_url=changed)

--- bookShop/settings.py
+++ bookShop/settings.py
@@ -152,20 +152,15 @@ RAZORPAY_SECRET = os.environ.get("RAZORPAY_SECRET")
 
 
 
+# # Celery settings
+# CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
+# CELERY_TIMEZONE = "Asia/Kolkata"
+# CELERY_RESULT_EXTENDED = True
+# CELERY_RESULT_BACKEND = 'django-db'
+
+
 # Celery settings
-CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
+CELERY_BROKER_URL = "redis://172.31.35.153:6379/0"
 CELERY_TIMEZONE = "Asia/Kolkata"
 CELERY_RESULT_EXTENDED = True
 CELERY_RESULT_BACKEND = 'django-db'
-
-
-# Celery Configuration Options
-# CELERY_BROKER_URL = f"redis://127.0.0.1:6379"
-# BROKER_URL = f"redis://127.0.0.1:6379"
-# CELERY_TIMEZONE = "Asia/Kolkata"
-# CELERY_TASK_TRACK_STARTED = True
-# CELERY_TASK_TIME_LIMIT = 30 * 60
-# CELERY_RESULT_EXTENDED = True
-# CELERY_worker_state_db = True
-# CELERY_result_persistent=True
-# CELERY_RESULT_BACKEND = 'django-db'
